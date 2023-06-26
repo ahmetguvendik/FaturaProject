14fabe58eb32fc02562708d5c0998424068299b4
Ahmet Güvendik <ahmetguvendik01@gmail.com>
2023-06-26 13:41:04 +0300

    FirstCommit

---
+++ FaturaProject/FaturaProject/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+﻿namespace FaturaProject.Models;
+
+public class ErrorViewModel
+{
+    public string? RequestId { get; set; }
+
+    public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+}
+
---
+++ FaturaProject/Presentation/Fatura.Presentation/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+﻿namespace Fatura.Presentation.Models;
+
+public class ErrorViewModel
+{
+    public string? RequestId { get; set; }
+
+    public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+}
+
